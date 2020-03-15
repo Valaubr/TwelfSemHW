50ad2d05df32d1b8ba91c63ccc23af2b5cf21a79
Valaubr <39511164+Valaubr@users.noreply.github.com>
2020-03-15 21:19:09 +0300

    Update BinaryTree.java

--- src/main/java/Interfaces/BinaryTree.java
+++ src/main/java/Interfaces/BinaryTree.java
@@ -1,13 +1,9 @@
 package Interfaces;
 
 public interface BinaryTree {
-    public void add(Integer value);
+    public boolean add(Integer value);
 
-    public void delete(Integer value);
+    public boolean delete(Integer value);
 
     public boolean search(Integer value);
-
-    public void draw();
-
-    public String[] getNodeAroundCurrent(Integer value);
 }
